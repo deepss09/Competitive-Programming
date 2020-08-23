9cfaf586a3392834d42ea32b3463b643f67a290f
Deepali Budhiraja <deepalibudhiraja@gmail.com>
2020-08-23 23:44:07 +0530

    Created

---
+++ CodeForces/Problem460A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem460A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int ans = 0;
+		System.out.println(n+((n-1)/(m-1)));
+	    
+			
+		}
+
+	}
+
