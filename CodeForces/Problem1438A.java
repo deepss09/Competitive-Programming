a9482e2360f9b792b3f68490d24b1f085c4c0f38
Deepali Budhiraja <deepalibudhiraja@gmail.com>
2020-11-20 19:28:40 +0530

    Created

---
+++ CodeForces/Problem1438A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem1438A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int sum = 0;
+		while(t-->0) {
+			int n = sc.nextInt();
+			for(int i=0;i<n;i++) {
+			 System.out.print(1+" ");
+			 System.out.println();
+			}
+		}
+	}
+}
