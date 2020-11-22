b02470236dafc06b5517bb69d3d8f92b0a81000f
Deepali Budhiraja <deepalibudhiraja@gmail.com>
2020-11-22 22:11:06 +0530

    Created

---
+++ CodeForces/Problem1451A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem1451A {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-->0) {
+			int n = s.nextInt();
+//			int c = 1;
+			System.out.println(Math.min(n%2,n-1));
+			}
+		}
+	
+
+}
+
