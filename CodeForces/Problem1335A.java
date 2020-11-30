8fda46ac9357d5def81faf99fded4562fd0e7b80
Deepali Budhiraja <deepalibudhiraja@gmail.com>
2020-11-30 17:50:25 +0530

    Created

---
+++ CodeForces/Problem1335A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem1335A {
+
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 int t = sc.nextInt();
+	 while(t-->0) {
+		 int n = sc.nextInt();
+		 int a = 0;
+		 int b = 0; 
+				 System.out.println((n-1)/2);
+			 }
+		 }
+	 }
+
+
