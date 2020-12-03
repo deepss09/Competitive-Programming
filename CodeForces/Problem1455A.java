81e03872d4cfc542575f92c5bd646175c334e144
Deepali Budhiraja <deepalibudhiraja@gmail.com>
2020-12-03 18:48:12 +0530

    Created

---
+++ CodeForces/Problem1455A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem1455A {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+			String n = sc.next();
+			int count = 0;
+//			for(int i=0;i<n.length();i++) {
+				System.out.println(n.length());
+			}
+		}
+
+
+}
